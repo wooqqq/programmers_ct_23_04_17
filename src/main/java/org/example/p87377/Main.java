e356706c3fd9a5aa5991145fbbf37b3f4ffa156e
깃wooqqq <sge0225@naver.com>
2023-04-17 13:50:15 +0900

    feat : 교점 구현

--- src/main/java/org/example/p87377/Main.java
+++ src/main/java/org/example/p87377/Main.java
@@ -9,7 +9,30 @@ class Solution {
         return answer;
     }
 
-    public boolean intersection(int[] line1, int[] line2) {
-        return null;
+    public long intersection(int[] line1, int[] line2) {
+        // Ax + By + E = 0
+        long A = line1[0];
+        long B = line1[1];
+        long E = line1[2];
+
+        // Cx + Dy + F = 0
+        long C = line2[0];
+        long D = line2[1];
+        long F = line2[2];
+
+        // 아래와 같은 경우 평행해서 교점이 없다.
+        if (A * D - B * C == 0) {
+            return null;
+        }
+
+        double x = (double) (B * F - E * D) / (A * D - B * C);
+        double y = (double) (E * C - A * F) / (A * D - B * C);
+
+        // 문제에서 정수좌표만 구하라고 했다.
+        if (x != (long) x) return null;
+        // 문제에서 정수좌표만 구하라고 했다.
+        if (y != (long) y) return null;
+
+        return new long[]{(long) x, (long) y};
     }
 }
--- src/test/java/org/example/p87377/Tests.java
+++ src/test/java/org/example/p87377/Tests.java
@@ -3,17 +3,45 @@ package org.example.p87377;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.management.relation.RoleUnresolvedList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Tests {
     @Test
     @DisplayName("교점 [[1, -1, 0], [2, -1, 0]]")
-    void t001() {
+    void t1() {
         assertThat(
                 new Solution().intersection(
                         new int[]{1, -1, 0},
                         new int[]{2, -1, 0}
                 )
-        ).isEqualTo(new int[]{0, 0});
+        ).isEqualTo(new long[]{0, 0});
+    }
+
+    @Test
+    @DisplayName("교점, [[1, -1, 0], [4, -1, 0]]")
+    void t1_2() {
+        assertThat(
+                new Solution().intersection(
+                        new int[]{1, -1, 0},
+                        new int[]{4, -1, 0}
+                )
+        ).isEqualTo(
+                new long[]{0, 0}
+        );
+    }
+
+    @Test
+    @DisplayName("교점, [[2, -1, 0], [4, -1, 0]]")
+    void t1_3() {
+        assertThat(
+                new Solution().intersection(
+                        new int[]{2, -1, 0},
+                        new int[]{4, -1, 0}
+                )
+        ).isEqualTo(
+                new long[]{0, 0}
+        );
     }
 }
