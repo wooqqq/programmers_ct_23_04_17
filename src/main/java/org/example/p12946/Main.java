fe87f32d5e124d38895c5f3b6adb50664529189e
wooqqq <sge0225@naver.com>
2023-04-19 13:04:56 +0900

    fix : 디버그

--- src/main/java/org/example/p12946/Main.java
+++ src/main/java/org/example/p12946/Main.java
@@ -16,23 +16,31 @@ class Hanoi {
     private final List<int[]> paths = new ArrayList<>();
 
     public Hanoi(int from, int to, int n) {
-        calculate(from, to, n);
+        calculate(from, to, n, 1);
     }
 
     // from : 시작점
     // to : 목적지
     // n : 옮길 원판 개수
     // empty : from 도 아니고 to 도 아닌 기둥 번호
-    private void calculate(int from, int to, int n) {
+    private void calculate(int from, int to, int n, int depth) {
+        String debugLineHead = "\t".repeat(depth - 1) + "(%d) => (%d), %d개 옮기기".formatted(from, to, n);
+
+        System.out.println(debugLineHead + " - 시작");
+
         if (n == 1) {
             addPath(from, to);
+
+            System.out.println(debugLineHead + " - 끝");
             return;
         }
         int empty = 6 - from - to;
 
-        calculate(from, empty, n - 1);
-        calculate(from, to, 1);
-        calculate(empty, to, n - 1);
+        calculate(from, empty, n - 1, depth + 1);
+        calculate(from, to, 1, depth + 1);
+        calculate(empty, to, n - 1, depth + 1);
+
+        System.out.println(debugLineHead + " - 끝");
     }
 
     private void addPath(int from, int to) {
