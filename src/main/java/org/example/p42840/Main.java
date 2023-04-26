75af021f399681b33b07c714d871164826bc9230
wooqqq <sge0225@naver.com>
2023-04-26 13:18:13 +0900

    feat : 수포자들이 받은 점수가 최고점수와 동일한 수포자들의 번호만 추려서 배열에 담기

--- src/main/java/org/example/p42840/Main.java
+++ src/main/java/org/example/p42840/Main.java
@@ -1,6 +1,7 @@
 package org.example.p42840;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
@@ -23,12 +24,16 @@ public class Main {
 
 class Solution {
     public int[] solution(int[] answers) {
+        // 최고점수 얻고
         int maxPoint = Stream.of(scoreOf1(answers), scoreOf2(answers), scoreOf3(answers))
                 .mapToInt(Integer::intValue)
                 .max()
                 .getAsInt();
 
-        return new int[]{maxPoint};
+        // 각각의 방법으로 했을 때의 점수를 얻은 후 그것이 최고점수이면 배열에 수포자 번호를 담는다.
+        return IntStream.rangeClosed(1, 3)
+                .filter(patternNo -> maxPoint == scoreOf(answers, patternNo))
+                .toArray();
     }
 
     private int scoreOf1(int[] answers) {
@@ -41,6 +46,13 @@ class Solution {
 
     private int scoreOf3(int[] answers) {
         return scoreOf(answers, new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5});
+    }
+
+    private int scoreOf(int[] answers, int patternNo) {
+        if (patternNo == 1) return scoreOf1(answers);
+        if (patternNo == 2) return scoreOf2(answers);
+
+        return scoreOf3(answers);
     }
 
     private int scoreOf(int[] answers, int[] pattern) {
