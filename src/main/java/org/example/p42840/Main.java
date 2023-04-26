dea65c69bee5e6a352f2612a7e2f6e36fc227b2a
wooqqq <sge0225@naver.com>
2023-04-26 13:11:04 +0900

    fix : 중복 제거

--- src/main/java/org/example/p42840/Main.java
+++ src/main/java/org/example/p42840/Main.java
@@ -31,36 +31,18 @@ class Solution {
     }
 
     private int scoreOf1(int[] answers) {
-        int[] pattern = {1, 2, 3, 4, 5};
-
-        int score = 0;
-
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == pattern[i % pattern.length]) {
-                score++;
-            }
-        }
-
-        return score;
+        return scoreOf(answers, new int[]{1, 2, 3, 4, 5});
     }
 
     private int scoreOf2(int[] answers) {
-        int[] pattern = {2, 1, 2, 3, 2, 4, 2, 5};
-
-        int score = 0;
-
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == pattern[i % pattern.length]) {
-                score++;
-            }
-        }
-
-        return score;
+        return scoreOf(answers, new int[]{2, 1, 2, 3, 2, 4, 2, 5});
     }
 
     private int scoreOf3(int[] answers) {
-        int[] pattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        return scoreOf(answers, new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5});
+    }
 
+    private int scoreOf(int[] answers, int[] pattern) {
         int score = 0;
 
         for (int i = 0; i < answers.length; i++) {
