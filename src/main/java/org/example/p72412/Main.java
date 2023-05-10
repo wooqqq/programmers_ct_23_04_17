8bb768fe299a824036747fe181aea18bbf2b9ab9
wooqqq <sge0225@naver.com>
2023-05-10 11:50:13 +0900

    완성

--- src/main/java/org/example/p72412/Main.java
+++ src/main/java/org/example/p72412/Main.java
@@ -1,8 +1,9 @@
 package org.example.p72412;
 
-import com.sun.nio.sctp.AbstractNotificationHandler;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 }
@@ -15,10 +16,37 @@ class Solution {
 
     Map<String, List<Integer>> buildScoresMap(String[] info) {
         Map<String, List<Integer>> scoresMap = new HashMap<>();
-        scoresMap.put("java - - -", List.of(80, 150));
-        scoresMap.put("python frontend senior chicken", List.of(150, 210));
-        scoresMap.put("python - - -", List.of(50, 150, 210));
-        scoresMap.put("- - senior -", List.of(50, 150, 210, 260));
+
+        for (String infoRow : info) {
+            String[] infoRowBits = infoRow.split(" ");
+
+            String languageTypeCode = infoRowBits[0];
+            String jobGroupTypeCode = infoRowBits[1];
+            String careerTypeCode = infoRowBits[2];
+            String foodTypeCode = infoRowBits[3];
+            int score = Integer.parseInt(infoRowBits[4]);
+
+            String[] languageTypeCodes = new String[]{"-", languageTypeCode};
+            String[] jobGroupTypeCodes = new String[]{"-", jobGroupTypeCode};
+            String[] careerTypeCodes = new String[]{"-", careerTypeCode};
+            String[] foodTypeCodes = new String[]{"-", foodTypeCode};
+
+            for (String languageTypeCode_ : languageTypeCodes) {
+                for (String jobGroupTypeCode_ : jobGroupTypeCodes) {
+                    for (String careerTypeCode_ : careerTypeCodes) {
+                        for (String foodTypeCode_ : foodTypeCodes) {
+                            String key = languageTypeCode_ + " " + jobGroupTypeCode_ + " " + careerTypeCode_ + " " + foodTypeCode_;
+
+                            scoresMap.putIfAbsent(key, new ArrayList<>()); // key가 없으면 새로운 ArrayList를 value로 넣어줌
+                            scoresMap.get(key).add(score);
+                        }
+                    }
+                }
+            }
+        }
+
+        // 정렬
+        scoresMap.forEach((key, scores) -> scores.sort(Integer::compareTo));
 
         return scoresMap;
     }
