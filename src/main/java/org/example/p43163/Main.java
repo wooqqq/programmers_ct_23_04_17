39db93079fc75af64fa2781c76450a85eaa75e06
wooqqq <sge0225@naver.com>
2023-05-16 09:16:52 +0900

    무식하고 엉성한 접근

--- src/main/java/org/example/p43163/Main.java
+++ src/main/java/org/example/p43163/Main.java
@@ -1,11 +1,46 @@
 package org.example.p43163;
 
+import java.util.Arrays;
+
 public class Main {
 }
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        return 4;
+        boolean exists = Arrays.stream(words)
+                .filter(e -> e.equals(target))
+                .count() == 1L;
+
+        if (!exists) return 0;
+
+        boolean[] visited = new boolean[words.length];
+
+        String current = begin;
+
+        int whileCount = 0;
+        int depth = 0;
+
+        while (true) {
+            whileCount++;
+
+            for (int i = 0; i < words.length; i++) {
+                if (visited[i]) continue;
+
+                if (isConvertible(current, words[i])) {
+                    visited[i] = true;
+
+                    if (words[i].equals(target)) return depth;
+
+                    current = words[i];
+                    System.out.println(current);
+                    depth++;
+                }
+            }
+
+            if (whileCount > 1000) break;
+        }
+
+        return depth;
     }
 
     public boolean isConvertible(String word1, String word2) {
